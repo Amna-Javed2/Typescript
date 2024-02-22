8c37ae3521d360dc15d926e216081693e542cc27
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:43:17 -0800

    Delete 06-stripping-name/famous-quote-2.js

--- 06-stripping-name/famous-quote-2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let famous_person = 'Albert Einstein';
-let message = '“A person who never made a mistake never tried anything new.”';
-console.log(`${famous_person} once said, ${message}`);
