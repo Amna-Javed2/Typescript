a87b2b3467f95a9e5a8236cc49f0af419d02a004
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:33:50 -0800

    Add files via upload

---
+++ 13-your own array or transpot/index.js
@@ -0,0 +1,4 @@
+var transportation = ['bike', 'cycle', 'train', 'bus'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
---
+++ 13-your own array or transpot/index.ts
@@ -0,0 +1,5 @@
+let transportation :string[]=['bike','cycle','train','bus'];
+for(let i=0; i<transportation.length; i++)
+{
+    console.log('I would like to own a '+ transportation[i]);
+}
