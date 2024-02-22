6b7108116eb5dfcfa0b35ae2f90be18efd131402
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:29:55 -0800

    Add files via upload

---
+++ 09-adding comments/index.js
@@ -0,0 +1,4 @@
+//program written by : Amna
+//current date:20-2-2024
+//this program will write Hello World
+console.log('Hello World');
---
+++ 09-adding comments/index1.js
@@ -0,0 +1,3 @@
+//this program written by:amna
+//this program will add two digits
+console.log(2 + 2);
