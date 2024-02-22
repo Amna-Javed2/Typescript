7f38e65e5425c063f5e187ab292466696b21b5ae
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:49:20 -0800

    Add files via upload

---
+++ 06-stripping-name/index.js
@@ -0,0 +1,4 @@
+var Name = '    \n\tjavedstore\n\t    ';
+console.log(Name);
+var Name2 = Name.trim();
+console.log(Name2);
---
+++ 06-stripping-name/index.ts
@@ -0,0 +1,4 @@
+const Name :string='    \n\tjavedstore\n\t    ';
+console.log(Name);
+const Name2 :string=Name.trim();
+console.log(Name2)
