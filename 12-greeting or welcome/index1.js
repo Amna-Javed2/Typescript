6f61b47781e76430e9635154f9db98ded2ce21fe
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:32:59 -0800

    Add files via upload

---
+++ 12-greeting or welcome/index1.js
@@ -0,0 +1,5 @@
+var names = ['kakar', 'kakri', 'kamran', 'tessori'];
+var message = 'welocome to typescript class:';
+for (var i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12-greeting or welcome/index1.ts
@@ -0,0 +1,9 @@
+let names :string[]=['kakar','kakri','kamran','tessori'];
+
+const message :string='welocome to typescript class:'
+
+for( let i=0; i<names.length; i++){
+
+   console.log(message + names[i]);
+    
+}
