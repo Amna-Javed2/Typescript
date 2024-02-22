1bb3dffb0fd090ee65055fac44a01a3f7d1d6467
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:31:45 -0800

    Add files via upload

---
+++ 11-print name of friends in array/index1.js
@@ -0,0 +1,4 @@
+var names = ['a', 'b', 'c', 'd'];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11-print name of friends in array/index1.ts
@@ -0,0 +1,6 @@
+let names :string[]=['a','b','c','d'];
+for( let i=0; i<names.length; i++){
+
+   console.log(names[i]);
+    
+}
