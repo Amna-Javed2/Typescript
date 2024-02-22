959b5ae72250359af21f9e3994f76c4473d8964d
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:30:41 -0800

    Add files via upload

---
+++ 10-favourite number/index.js
@@ -0,0 +1,3 @@
+var favourite_number = 2;
+var message = 'here is your favourite number' + favourite_number;
+console.log(message);
---
+++ 10-favourite number/index.ts
@@ -0,0 +1,3 @@
+const favourite_number :number=2;
+const message : string='here is your favourite number' + favourite_number;
+console.log(message);
