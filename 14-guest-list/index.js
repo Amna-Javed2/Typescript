37ef97ec6805fd0b7722e24c5b214a423924d86d
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-02-22 00:36:47 -0800

    Add files via upload

---
+++ 14-guest-list/index.js
@@ -0,0 +1,4 @@
+var guest_list = ['Imran Khan', 'Nawaz Sharif', 'zardari'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Dear Mr.' + guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n Thank you!');
+}
---
+++ 14-guest-list/index.ts
@@ -0,0 +1,6 @@
+let guest_list :string[]=['Imran Khan','Nawaz Sharif','zardari'];
+for(let i=0; i<guest_list.length; i++)
+{
+    console.log('Dear Mr.' + guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\n Thank you!'); 
+    
+}
